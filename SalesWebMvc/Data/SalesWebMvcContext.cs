d3da21014265f18a4dd1d54dca008443c93aee4b
emerson07 <emerson.tamura07@gmail.com>
2020-05-10 20:56:49 -0300

    CRUD Scaffolding

---
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Controllers;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesWebMvcContext : DbContext
+    {
+        public SalesWebMvcContext (DbContextOptions<SalesWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMvc.Controllers.Department> Department { get; set; }
+    }
+}
